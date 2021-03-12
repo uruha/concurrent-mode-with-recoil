8ff55b57c8dff4745963c5ffc9964dfce60e23b8
chiba-kotaro <chiba-kotaro@PC-0001199.local>
2021-03-12 11:53:08 +0900

    add: dashboard with concurrent mode and recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,78 @@
-import React from 'react';
+import React, { Suspense, useState } from 'react';
+import { atom, selector, RecoilState, useRecoilValue, RecoilValueReadOnly } from 'recoil';
 
-const App = () => <div>Hello</div>;
+type UserId = string;
+type Profile = {
+  name: string
+};
+interface UserDataResource {
+  profile: RecoilValueReadOnly<Profile>;
+}
+
+const userIdState = atom<UserId>({
+  key: 'UserId',
+  default: 'u001'
+});
+
+/** Promise mock */
+const fetchProfile = (userId: UserId) => {
+  return new Promise<Profile>(resolve => {
+    setTimeout(() => {
+      resolve({
+        name: `ID: ${userId}: Ringo Starr`
+      });
+    }, 1000);
+  });
+};
+
+/** like adaptor interface */
+const userDataFetcher = (userIdState: RecoilState<UserId>) => {
+  const userProfileState = selector({
+    key: 'UserProfile',
+    get: async ({ get }) => {
+      const response = await fetchProfile(get(userIdState));
+      return response;
+    }
+  });
+
+  return {
+    profile: userProfileState
+  };
+};
+
+const ProfileDetails: React.VFC<{ resource: UserDataResource }> = ({ resource }) => {
+  const profile = useRecoilValue(resource.profile);
+  return <h1>{profile.name}</h1>;
+};
+
+
+const initialResource = userDataFetcher(userIdState);
+
+const Dashboard = () => {
+  const [resource, setResource] = useState(initialResource);
+
+  const handleRefreshData = () => {
+    setResource(userDataFetcher(userIdState));
+  }
+
+  return (
+    <>
+      <button onClick={handleRefreshData}>
+        Refresh
+      </button>
+      <Suspense
+        fallback={<p>Loading profile...</p>}
+      >
+        <ProfileDetails resource={resource} />
+      </Suspense>
+    </>
+  );
+};
+
+const App = () => (
+  <>
+    <Dashboard />
+  </>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 ReactDOM.unstable_createRoot(
   document.getElementById('root') as Element
 ).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
